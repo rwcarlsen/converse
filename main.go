8a0d2815e9f14cceb301a73c73cabd0a5095e365
Robert Carlsen <rwcarlsen@gmail.com>
2017-04-09 18:45:37 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+const usage = `converse [flags...] <subcmd>
+`
+
+func init() { log.SetFlags(log.Lshortfile) }
+
+func main() {
+	flag.Parse()
+	if flag.NArg() < 1 {
+		log.Fatal(usage)
+	}
+
+	switch cmd := flag.Arg(0); cmd {
+	case "list":
+		list(cmd, flag.Args()[1:])
+	default:
+		log.Fatalf("unrecognized command '%v'", cmd)
+	}
+}
+
+func parseMsgNum(fname string) (int, error) {
+	i := strings.Index(fname, ".")
+	s := fname[3:i]
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return -1, err
+	}
+	return i, nil
+}
+
+func sortMsgs(names []string) {
+	sort.Slice(names, func(i, j int) bool {
+		ni, err := parseMsgNum(names[i])
+		check(err)
+		nj, err := parseMsgNum(names[j])
+		check(err)
+		return ni < nj
+	})
+}
+
+func printUsage(cmd, usage, msg string) {
+	log.Fatalf("%v. Usage:\n    %v %v\n", msg, cmd, usage)
+}
+
+func list(cmd string, args []string) {
+	const usage = `<conversation-directory>`
+	fs := flag.NewFlagSet(cmd, flag.ContinueOnError)
+	err := fs.Parse(args)
+	check(err)
+
+	if fs.NArg() != 1 {
+		printUsage(cmd, usage, "Wrong number of arguments")
+	}
+
+	convName := fs.Arg(0)
+	f, err := os.Open(convName)
+	check(err)
+	defer f.Close()
+
+	names, err := f.Readdirnames(0)
+	check(err)
+	sortMsgs(names)
+
+	for _, name := range names {
+		data, err := ioutil.ReadFile(filepath.Join(convName, name))
+		check(err)
+		fmt.Println(string(data))
+	}
+}
---
+++ notes.md
@@ -0,0 +1,18 @@
+
+* Each user can have a "conversations" directory in a standard location in
+  their upspin tree.
+
+* Each conversation thread is its own directory in the main "conversations"
+  directory.
+
+* Each message/reply in the conversation is a file named "msg1.txt",
+  "msg2.txt", and so on.
+
+* Edits/modifications to a message are "msg1.1.txt" for the 1st modified
+  revision version of message 1, "msg1.2.txt" for the 2nd modified revision
+  and so on.
+
+* Participants in a conversation are controlled by the upspin Access
+  file.  
+
+* 
